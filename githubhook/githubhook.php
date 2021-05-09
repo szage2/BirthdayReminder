64fd55a2bc8565effea960159a15f28b8ae45817
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-09 10:53:41 +0200

    Changed file name for saved requests

--- githubhook/githubhook.php
+++ githubhook/githubhook.php
@@ -1,7 +1,7 @@
 <?php
 $postBody = $_POST;
 
-$f = fopen("../logs/requests.json", "a") or die ("Unable to open file!");
+$f = fopen("../logs/requests_birthday_reminder.json", "a") or die ("Unable to open file!");
 fwrite($f, json_encode($_POST, JSON_PRETTY_PRINT));
 fclose($f);
 ?>
