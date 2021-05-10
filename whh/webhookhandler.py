b2091e927a6adf5de873cd17b575d2ac8f0baa7c
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-10 14:57:50 +0200

    Moved from camelcase to lowercase to better fit SonarCloud patterns

--- whh/webhookhandler.py
+++ whh/webhookhandler.py
@@ -14,12 +14,12 @@ def _main():
         o = repo.remotes.origin
         o.pull()
 
-        dockerClient = docker.from_env()
-        dockerClient.containers.get("birthdayreminder").stop()
-        dockerClient.containers.get("birthdayreminder").remove()
-        dockerClient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
-        dockerClient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081"}, name="birthdayreminder", restart_policy={"Name": "always", "MaximumRetryCount": 5})
-        dockerClient.containers.prune()
+        dockerclient = docker.from_env()
+        dockerclient.containers.get("birthdayreminder").stop()
+        dockerclient.containers.get("birthdayreminder").remove()
+        dockerclient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
+        dockerclient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081"}, name="birthdayreminder", restart_policy={"Name": "always", "MaximumRetryCount": 5})
+        dockerclient.containers.prune()
 
 if __name__ == '__main__':
     _main()
