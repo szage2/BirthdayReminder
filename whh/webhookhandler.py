97fbaa637a77845bbc5d79a92b2ef9b55f61a425
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-10 11:19:52 +0200

    Supposedly creating new build of BirthdayReminder, if there's an update in the repo

--- whh/webhookhandler.py
+++ whh/webhookhandler.py
@@ -1,5 +1,6 @@
 import inotify.adapters
 import git
+import docker
 
 def _main():
     i = inotify.adapters.Inotify()
@@ -12,6 +13,9 @@ def _main():
         repo = git.Repo("/home/pi/BirthdayReminder")
         o = repo.remotes.origin
         o.pull()
+        dockerClient = docker.from_env()
+        birthdayReminderImage = dockerClient.images.build("/home/pi/BirthdayReminder",tag="birthdayreminder")
+        dockerClient.containers.run(birthdayReminderImage, detach=True, port="8081:8081", name="birthdayreminder")
 
 if __name__ == '__main__':
     _main()
