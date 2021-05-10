6f71d21bca53e71e297694e23029545319522949
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-10 12:42:42 +0200

    Adding restart policy to whh

--- whh/webhookhandler.py
+++ whh/webhookhandler.py
@@ -18,7 +18,7 @@ def _main():
         dockerClient.containers.get("birthdayreminder").stop()
         dockerClient.containers.get("birthdayreminder").remove()
         dockerClient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
-        dockerClient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081"}, name="birthdayreminder")
+        dockerClient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081"}, name="birthdayreminder", restart_policy={"Name": "always", "MaximumRetryCount": 5})
         dockerClient.containers.prune()
 
 if __name__ == '__main__':
