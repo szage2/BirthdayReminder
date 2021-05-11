34ad215b401abde7da74b90977b31f2e8502ed25
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-11 14:30:08 +0200

    Rollback

--- whh/webhookhandler.py
+++ whh/webhookhandler.py
@@ -5,9 +5,10 @@ import docker
 def _main():
     i = inotify.adapters.Inotify()
 
-    i.add_watch('/home/pi/requests_birthday_reminder.json')
-    try:
-        while (True):
+    while (True):
+        try:
+            i.add_watch('/home/pi/requests_birthday_reminder.json')
+
             for event in i.event_gen(yield_nones=False):
                 (_, type_names, path, filename) = event
 
@@ -21,8 +22,8 @@ def _main():
                 dockerclient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
                 dockerclient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081"}, name="birthdayreminder", restart_policy={"Name": "always", "MaximumRetryCount": 5})
                 dockerclient.containers.prune()
-    except:
-        print("Error while running whh")
+        except:
+            print("Error while running whh")
 
 if __name__ == '__main__':
     _main()
