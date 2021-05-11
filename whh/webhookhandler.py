684095b998a68a72219801d022349b944d895a51
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-11 14:34:09 +0200

    Updated whh

--- whh/webhookhandler.py
+++ whh/webhookhandler.py
@@ -4,11 +4,10 @@ import docker
 
 def _main():
     i = inotify.adapters.Inotify()
+    i.add_watch('/home/pi/requests_birthday_reminder.json')
 
     while (True):
         try:
-            i.add_watch('/home/pi/requests_birthday_reminder.json')
-
             for event in i.event_gen(yield_nones=False):
                 (_, type_names, path, filename) = event
 
