c7e0949a0fef181a5cf3ba5f0a2250ba5a3e2af9
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-11 14:10:15 +0200

    Added error handling and loop to keep whh up and running

--- whh/webhookhandler.py
+++ whh/webhookhandler.py
@@ -5,21 +5,25 @@ import docker
 def _main():
     i = inotify.adapters.Inotify()
 
-    i.add_watch('/home/pi/requests_birthday_reminder.json')
+    while (True):
+        try:
+            i.add_watch('/home/pi/requests_birthday_reminder.json')
 
-    for event in i.event_gen(yield_nones=False):
-        (_, type_names, path, filename) = event
+            for event in i.event_gen(yield_nones=False):
+                (_, type_names, path, filename) = event
 
-        repo = git.Repo("/home/pi/BirthdayReminder")
-        o = repo.remotes.origin
-        o.pull()
+                repo = git.Repo("/home/pi/BirthdayReminder")
+                o = repo.remotes.origin
+                o.pull()
 
-        dockerclient = docker.from_env()
-        dockerclient.containers.get("birthdayreminder").stop()
-        dockerclient.containers.get("birthdayreminder").remove()
-        dockerclient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
-        dockerclient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081"}, name="birthdayreminder", restart_policy={"Name": "always", "MaximumRetryCount": 5})
-        dockerclient.containers.prune()
+                dockerclient = docker.from_env()
+                dockerclient.containers.get("birthdayreminder").stop()
+                dockerclient.containers.get("birthdayreminder").remove()
+                dockerclient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
+                dockerclient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081"}, name="birthdayreminder", restart_policy={"Name": "always", "MaximumRetryCount": 5})
+                dockerclient.containers.prune()
+        except:
+            print("Error while running whh")
 
 if __name__ == '__main__':
     _main()
