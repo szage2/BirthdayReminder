9902d618ac42b162ce9f684a630033f362f19549
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-13 17:12:02 +0200

    Updating whh, as from this point on the OS is responsible for respawn.

--- whh/webhookhandler.py
+++ whh/webhookhandler.py
@@ -7,28 +7,25 @@ def _main():
     i.add_watch('/home/pi/requests_birthday_reminder.json')
 
     while (True):
-        try:
-            for event in i.event_gen(yield_nones=False):
-                (_, type_names, path, filename) = event
+        for event in i.event_gen(yield_nones=False):
+            (_, type_names, path, filename) = event
 
-                repo = git.Repo("/home/pi/BirthdayReminder")
-                o = repo.remotes.origin
-                o.pull()
+            repo = git.Repo("/home/pi/BirthdayReminder")
+            o = repo.remotes.origin
+            o.pull()
 
-                dockerclient = docker.from_env()
-                try:
-                    oldbirthdayreminder = dockerclient.containers.get("birthdayreminder")
-                except Exception as e:
-                    print(e)
+            dockerclient = docker.from_env()
+            try:
+                oldbirthdayreminder = dockerclient.containers.get("birthdayreminder")
+            except Exception as e:
+                print(e)
 
-                if ('oldbirthdayreminder' in locals()):
-                    oldbirthdayreminder.stop()
-                    oldbirthdayreminder.remove()
-                dockerclient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
-                dockerclient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081"}, name="birthdayreminder", restart_policy={"Name": "always"})
-                dockerclient.containers.prune()
-        except Exception as e:
-            print(e)
+            if ('oldbirthdayreminder' in locals()):
+                oldbirthdayreminder.stop()
+                oldbirthdayreminder.remove()
+            dockerclient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
+            dockerclient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081"}, name="birthdayreminder", restart_policy={"Name": "always"})
+            dockerclient.containers.prune()
 
 if __name__ == '__main__':
     _main()
