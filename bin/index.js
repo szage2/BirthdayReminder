3f2b7483cfcca4793c55edc2596082e5110f5046
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-14 22:19:45 +0200

    The code works, setting back the original file.

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,18 @@
-var http = require('http');
+var http = require("http");
+var fs = require("fs");
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8081); 
+  // Read index.html file and return the content
+  fs.readFile('index.html', function(err, data) {
+    // Send the HTTP header
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            // Send the response body as "Hello World"
+            res.end('Read html file...\n');
+  });
+
+}).listen(8081);
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
