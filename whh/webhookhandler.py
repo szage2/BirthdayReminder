0c3773df21b9a2577ece52c2c6e791dd2df9731b
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-15 00:21:01 +0200

    Adding while back to the code

--- whh/webhookhandler.py
+++ whh/webhookhandler.py
@@ -6,21 +6,22 @@ def _main():
     i = inotify.adapters.Inotify()
     i.add_watch('/home/pi/requests_birthday_reminder.json')
 
-    for event in i.event_gen(yield_nones=False):
-        (_, type_names, path, filename) = event
+    while (True):
+        for event in i.event_gen(yield_nones=False):
+            (_, type_names, path, filename) = event
 
-        repo = git.Repo("/home/pi/BirthdayReminder")
-        o = repo.remotes.origin
-        o.pull()
+            repo = git.Repo("/home/pi/BirthdayReminder")
+            o = repo.remotes.origin
+            o.pull()
 
-        dockerclient = docker.from_env()
+            dockerclient = docker.from_env()
 
-        if (oldbirthdayreminder = dockerclient.containers.get("birthdayreminder")):
-            oldbirthdayreminder.stop()
-            oldbirthdayreminder.remove()
-        dockerclient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
-        dockerclient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081","9229":"9229"}, name="birthdayreminder", restart_policy={"Name": "always"})
-        dockerclient.containers.prune()
+            if (oldbirthdayreminder = dockerclient.containers.get("birthdayreminder")):
+                oldbirthdayreminder.stop()
+                oldbirthdayreminder.remove()
+            dockerclient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
+            dockerclient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081","9229":"9229"}, name="birthdayreminder", restart_policy={"Name": "always"})
+            dockerclient.containers.prune()
 
 if __name__ == '__main__':
     _main()
