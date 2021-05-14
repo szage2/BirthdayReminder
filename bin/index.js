7816f4e004ca1ac467f0d5c1f5608b70f73db731
Me <me@mymail.com>
2021-05-14 20:10:44 +0200

    Test with HW

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,7 @@ http.createServer(function (request, response) {
 // Content Type: text/plain
         response.writeHead(200, {'Content-Type': 'text/plain'});
         // Send the response body as "Hello World"
-        response.end('Place your first message here...\n');
+        response.end('Hello World...\n');
 }).listen(8081);
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
