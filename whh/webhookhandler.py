ce6d8b0b3e46f4675e16432bc7613186ee98ac5c
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-15 00:29:45 +0200

    Removed loop again

--- whh/webhookhandler.py
+++ whh/webhookhandler.py
@@ -5,30 +5,28 @@ import docker
 def _main():
     i = inotify.adapters.Inotify()
     i.add_watch('/home/pi/requests_birthday_reminder.json')
+    for event in i.event_gen(yield_nones=False):
+        (_, type_names, path, filename) = event
 
-    while (True):
-        for event in i.event_gen(yield_nones=False):
-            (_, type_names, path, filename) = event
+        repo = git.Repo("/home/pi/BirthdayReminder")
+        o = repo.remotes.origin
+        o.pull()
 
-            repo = git.Repo("/home/pi/BirthdayReminder")
-            o = repo.remotes.origin
-            o.pull()
+        dockerclient = docker.from_env()
 
-            dockerclient = docker.from_env()
-
-            try:
-                oldbirthdayreminder = dockerclient.containers.get("birthdayreminder")
-            except docker.errors.NotFound:
-                dockerclient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
-                dockerclient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081","9229":"9229"}, name="birthdayreminder", restart_policy={"Name": "always"})
-                dockerclient.containers.prune()
-                continue
-
-            oldbirthdayreminder.stop()
-            oldbirthdayreminder.remove()
+        try:
+            oldbirthdayreminder = dockerclient.containers.get("birthdayreminder")
+        except docker.errors.NotFound:
             dockerclient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
             dockerclient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081","9229":"9229"}, name="birthdayreminder", restart_policy={"Name": "always"})
             dockerclient.containers.prune()
+            continue
+
+        oldbirthdayreminder.stop()
+        oldbirthdayreminder.remove()
+        dockerclient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
+        dockerclient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081","9229":"9229"}, name="birthdayreminder", restart_policy={"Name": "always"})
+        dockerclient.containers.prune()
 
 if __name__ == '__main__':
     _main()
