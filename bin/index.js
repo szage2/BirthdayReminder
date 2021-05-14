6832bac73af9d83fafc4b2f171c3889bde9c2680
Me <me@mymail.com>
2021-05-14 20:33:18 +0200

    Index.html file created, read it from node.js

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,16 @@
 var http = require("http");
 
 http.createServer(function (request, response) {
-// Send the HTTP header
-// HTTP Status: 200 : OK
-// Content Type: text/plain
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        // Send the response body as "Hello World"
-        response.end('Hello World...\n');
+  // Read index.html file and return the content
+  fs.readFile('index.html', function(err, data) {
+    // Send the HTTP header
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            // Send the response body as "Hello World"
+            response.end('Read html file...\n');
+  });
+
 }).listen(8081);
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
