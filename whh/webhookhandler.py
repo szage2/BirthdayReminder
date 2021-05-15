bc3532b484d64d26ef816337d43ac5e48c245d76
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-15 11:25:09 +0200

    Slight modifications, including SonarCloud recommendation on using const for local path

--- whh/webhookhandler.py
+++ whh/webhookhandler.py
@@ -2,13 +2,15 @@ import inotify.adapters
 import git
 import docker
 
+LOCALPATH = "/home/pi/BirthdayReminder"
+
 def _main():
     i = inotify.adapters.Inotify()
     i.add_watch('/home/pi/requests_birthday_reminder.json')
     for event in i.event_gen(yield_nones=False):
         (_, type_names, path, filename) = event
 
-        repo = git.Repo("/home/pi/BirthdayReminder")
+        repo = git.Repo(LOCALPATH)
         o = repo.remotes.origin
         o.pull()
 
@@ -16,17 +18,15 @@ def _main():
 
         try:
             oldbirthdayreminder = dockerclient.containers.get("birthdayreminder")
-        except docker.errors.NotFound:
-            dockerclient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
+            oldbirthdayreminder.stop()
+            oldbirthdayreminder.remove()
+            dockerclient.images.build(path=LOCALPATH,tag="birthdayreminder")
             dockerclient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081","9229":"9229"}, name="birthdayreminder", restart_policy={"Name": "always"})
             dockerclient.containers.prune()
-            continue
-
-        oldbirthdayreminder.stop()
-        oldbirthdayreminder.remove()
-        dockerclient.images.build(path="/home/pi/BirthdayReminder",tag="birthdayreminder")
-        dockerclient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081","9229":"9229"}, name="birthdayreminder", restart_policy={"Name": "always"})
-        dockerclient.containers.prune()
+        except docker.errors.NotFound:
+            dockerclient.images.build(path=LOCALPATH,tag="birthdayreminder")
+            dockerclient.containers.run(image="birthdayreminder", detach=True, ports={"8081":"8081","9229":"9229"}, name="birthdayreminder", restart_policy={"Name": "always"})
+            dockerclient.containers.prune()
 
 if __name__ == '__main__':
     _main()
