3a1c8319badd35c82d956c3614a9d4eb2eda3f3b
Me <me@mymail.com>
2021-05-15 14:13:53 +0200

    Delete test message

--- bin/index.js
+++ bin/index.js
@@ -10,7 +10,7 @@ http.createServer(function (req, res) {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(data);
             // Send the response body as "Hello World"
-            res.end('Read html file...\n');
+            res.end();
   });
 
 }).listen(8081);
