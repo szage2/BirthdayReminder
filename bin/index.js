b720e38a1e17a7915a5a0a231bdb04131d4a3d68
Me <me@mymail.com>
2021-05-16 11:51:42 +0200

    Adding public for testing purposes

--- bin/index.js
+++ bin/index.js
@@ -25,6 +25,8 @@ app.get('/style', function(req, res) {
   });
 });
 
+app.use('/public', express.static('public'));
+
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
